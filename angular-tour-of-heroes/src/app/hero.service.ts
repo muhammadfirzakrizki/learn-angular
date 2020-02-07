663f08e264d4b80ff67387aaa902af907383f34f
firzakrizki <firzakrizki@gmail.com>
2020-02-07 17:09:43 +0700

    lanjut lagi

---
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { MessageService } from './message.service';
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(
+    private messageService: MessageService
+  ) { }
+  getHeroes(): Observable<Hero[]> {
+    console.log('oke');
+    this.messageService.add('HeroService: fetched heroes');
+    return of (HEROES);
+  }
+}
